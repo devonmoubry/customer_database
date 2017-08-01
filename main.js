298e3d03a3879141fd6b21810bf74ec6c16d5daa
Devon Moubry <devon@moubry.com>
2017-07-31 22:36:29 -0500

    loop over data array to get values

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 console.log(customers);
+
+const customerInfo = customers.results
+for (var i = 0; i < customerInfo.length; i++) {
+  console.log(customerInfo[i].picture.thumbnail);
+  console.log(customerInfo[i].name.first);
+  console.log(customerInfo[i].name.last);
+  console.log(customerInfo[i].email);
+  console.log(customerInfo[i].location.street);
+  console.log(customerInfo[i].location.city);
+  console.log(customerInfo[i].location.state);
+  console.log(customerInfo[i].location.postcode);
+  console.log(customerInfo[i].phone);
+  console.log(customerInfo[i].id.value);
+}
